218636307e0727e2e3e0140638db3d71c7808a32
rsprince <sprince.richard@gmail.com>
2016-07-22 15:18:52 -0400

    Ignore sass cache, playing with tests

--- app/components/chrome/chrome-test.js
+++ app/components/chrome/chrome-test.js
@@ -1,17 +1,24 @@
 // Chrome Test
 'use strict';
 
-describe('myApp.Chrome module', function() {
+// describe('myApp.Chrome module', function() {
+//
+//   beforeEach(module('myApp.Chrome'));
+//
+//   describe('Chrome controller', function(){
+//
+//     it('should ....', inject(function($controller) {
+//       //spec body
+//       var ChromeCtrl = $controller('ChromeCtrl');
+//       expect(ChromeCtrl).toBeDefined();
+//     }));
+//
+//   });
+// });
 
-  beforeEach(module('myApp.Chrome'));
-
-  describe('Chrome controller', function(){
-
-    it('should ....', inject(function($controller) {
-      //spec body
-      var ChromeCtrl = $controller('ChromeCtrl');
-      expect(ChromeCtrl).toBeDefined();
-    }));
-
+describe('About Feature', function(){
+  it('adds two numbers together', function () {
+    expect(1+2).toEqual(3);
   });
+
 });
